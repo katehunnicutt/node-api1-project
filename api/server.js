a1231b2f0f1f700ef351b274e31a55f6867cc3b2
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-08-30 19:49:20 -0700

    done ish

--- api/server.js
+++ api/server.js
@@ -1,3 +1,122 @@
-// BUILD YOUR SERVER HERE
+//Gabe hi if you're reading this I cannot get my put request to work. 
+//No matter what is typed into the request I get a 404 error saying 
+//user id cannot be found
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// BUILD YOUR SERVER HERE
+const express = require("express")
+const User = require("./users/model")
+
+const server = express()
+server.use(express.json()) //v important line
+
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  console.log(req.body)
+  if(!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user"
+    })
+  } else {
+    User.insert(user)
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "error creating user",
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+  }
+})
+
+server.get("/api/users", (req, res) => {
+  User.find()
+  .then(users => {
+    res.json(users)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: "cannot get users dang that sucks",
+      err: err.message,
+      stack: err.stack,
+    })
+  })
+})
+
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+  .then(user => {
+    if(!user) {
+      res.status(404).json({
+        message: "The users with the specified ID does not exist"
+      })
+    }
+    res.json(user)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: "cannot get user you want dang that sucks",
+      err: err.message,
+      stack: err.stack,
+    })
+  })
+})
+
+server.delete("/api/users/:id", async (req, res) => {
+  try{
+    const possibleUser = await User.findById(req.params.id)
+  if(!possibleUser) {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist"
+    })
+  } else {
+    const deletedUser = await User.remove(possibleUser.id)
+    res.status(200).json(deletedUser)
+  }
+} catch (err) {
+  res.status(500).json({
+    message: "error deleting user dang again",
+    err: err.message,
+    stack: err.stack
+  })
+}
+})
+
+server.put("/api/users/:id", async (req, res) => {
+try {
+  const possibleUser = await User.findById(req.params.id)
+  if (!possibleUser) {
+    res.status(404).json({
+      message: "the user with the specified ID does not exist"
+    })
+  } else {
+    if(!req.body.name || !req.body.bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user"
+      })
+    } else {
+      const updatedUser = await User.update(
+        req.params.id, 
+        req.body
+        )
+      res.status(200).json(updatedUser)
+    }
+  }
+} catch (err) {
+  res.status(500).json({
+    message: "error deleting user dang again",
+    err: err.message,
+    stack: err.stack
+  })
+  }
+})
+
+
+server.use("*", (req, res) => {
+  res.status(404).json({message: "not found that sucks :/"})
+})
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-
+server.listen(port, () => {
+  console.log("om hey we're listening on", port)
+})
 //please just push this time thank you gods
